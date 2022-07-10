f84c31d0fff495bb8b672e1036620a5a8248131c
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-07-10 13:30:20 +0700

    Update cektrgo.sh

--- trojango/cektrgo.sh
+++ trojango/cektrgo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SL
+# wisnucokrosatrio
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -13,15 +13,6 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck You!!"
-exit 0
-fi
 clear
 echo -n > /tmp/other.txt
 data=( `cat /etc/trojan-go/akun.conf | grep '^###' | cut -d ' ' -f 2`);
@@ -61,6 +52,6 @@ oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";
 echo "$oth";
 echo "------------------------------------";
-echo "Script Mod By SL"
+echo "Script by wisnucokrosatrio"
 rm -rf /tmp/other.txt
 
