88562aef5e34a2547583f5cb7bb2411b2d6ac85f
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-07-10 14:59:44 +0700

    Update certv2ray.sh

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -13,15 +13,17 @@ sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
 curl https://get.acme.sh | sh
 alias acme.sh=~/.acme.sh/acme.sh
 /root/.acme.sh/acme.sh --upgrade --auto-upgrade
-/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-#/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-2048
-/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-256
+#/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-2048
+/root/.acme.sh/acme.sh --issue -d "${domain}" --standalone --keylength ec-384
 /root/.acme.sh/acme.sh --install-cert -d "${domain}" --ecc \
 --fullchain-file /etc/ssl/private/fullchain.pem \
 --key-file /etc/ssl/private/privkey.pem
 chown -R nobody:nogroup /etc/xray
-chmod 644 /etc/ssl/private/privkey.pem
 chmod 644 /etc/ssl/private/fullchain.pem
+chmod 644 /etc/ssl/private/privkey.pem
+cp /etc/ssl/private/fullchain.pem /etc/xray/xray.crt
+cp /etc/ssl/private/privkey.pem /etc/xray/xray.key
 systemctl start xray
 systemctl start xtrojan
 systemctl start xvless
