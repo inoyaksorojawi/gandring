648b33224baa0f1f6122454b0a05af736ae08be6
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-07-12 05:24:46 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -79,7 +79,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpntls 443
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpntls 2087
 Restart=on-failure
 
 [Install]
