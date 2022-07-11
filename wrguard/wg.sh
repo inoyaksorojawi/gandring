88beb5c00f6d475e82b6558a5f8d4646b540c74b
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-07-12 01:39:55 +0700

    Update wg.sh

--- wrguard/wg.sh
+++ wrguard/wg.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-# Check OS version
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-MYIP=$(curl -sS ipv4.icanhazip.com)
-clear
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
- Link Hosting Kalian
+# Debian 9 & 10 64bit
+# Ubuntu 18.04 & 20.04 bit
+# Centos 7 & 8 64bit 
+# By Akbar Maulana
+# My Telegram : https://t.me/Akbar218
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+ ==========================================
+#Link Hosting Kalian
 wisnuvpn="raw.githubusercontent.com/inoyaksorojawi/gandring/master/wrguard"
 
 # Check OS version
