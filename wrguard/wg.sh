223a80ec6c021461b84dac696233f2f190e4d839
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-07-11 23:51:42 +0700

    Update wg.sh
    Signed-off-by: inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>

--- wrguard/wg.sh
+++ wrguard/wg.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
-# Debian 9 & 10 64bit
-# Ubuntu 18.04 & 20.04 bit
-# Centos 7 & 8 64bit 
-# By wisnucokrosatrio
-# My Telegram : https://t.me/zerossl
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
-# ==================================================
-# Link Hosting Kalian
-wisnuvpn="raw.githubusercontent.com/inoyaksorojawi/gandring/master/wrguard"
+# Check OS version
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+ Link Hosting Kalian
+akbarvpn="raw.githubusercontent.com/AkbarStoreVPN/scriptvps/main/wireguard"
 
 # Check OS version
 if [[ -e /etc/debian_version ]]; then
@@ -37,9 +27,8 @@ if [[ -e /etc/wireguard/params ]]; then
 	echo -e "${Info} WireGuard sudah diinstal, silahkan ketik addwg untuk menambah client."
 	exit 1
 fi
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-${Info} instalasi Wireguard Script By zerossl
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+
+echo -e "${Info} Wireguard Script By Akbar Maulana"
 # Detect public IPv4 address and pre-fill for the user
 
 # Detect public interface and pre-fill for the user
@@ -62,55 +51,75 @@ elif [[ ${OS} == 'centos' ]]; then
 apt install iptables iptables-persistent -y
 # Make sure the directory exists (this does not seem the be the case on fedora)
 mkdir /etc/wireguard >/dev/null 2>&1
-
 chmod 600 -R /etc/wireguard/
 
+sleep 1
+echo -e "[ ${green}INFO$NC ] Generate key wireguard"
 SERVER_PRIV_KEY=$(wg genkey)
 SERVER_PUB_KEY=$(echo "$SERVER_PRIV_KEY" | wg pubkey)
 
 # Save WireGuard settings
-echo "SERVER_PUB_NIC=$SERVER_PUB_NIC
+sleep 1
+echo -e "[ ${green}INFO$NC ] Create config wireguard"
+cat> /etc/wireguard/params << END
+SERVER_PUB_NIC=$SERVER_PUB_NIC
 SERVER_WG_NIC=wg0
 SERVER_WG_IPV4=10.11.11.1
 SERVER_PORT=591
 SERVER_PRIV_KEY=$SERVER_PRIV_KEY
-SERVER_PUB_KEY=$SERVER_PUB_KEY" >/etc/wireguard/params
+SERVER_PUB_KEY=$SERVER_PUB_KEY
+END
 
 source /etc/wireguard/params
 
 # Add server interface
-echo "[Interface]
+sleep 1
+echo -e "[ ${green}INFO$NC ] Adding server interface"
+cat> /etc/wireguard/wg0.conf << END
+[Interface]
 Address = $SERVER_WG_IPV4/24
 ListenPort = $SERVER_PORT
 PrivateKey = $SERVER_PRIV_KEY
-PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE;
-PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE;" >>"/etc/wireguard/wg0.conf"
+PostUp = sudo iptables -A FORWARD -i wg0 -j ACCEPT; sudo iptables -t nat -A POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE;
+PostDown = sudo iptables -D FORWARD -i wg0 -j ACCEPT; sudo iptables -t nat -D POSTROUTING -o $SERVER_PUB_NIC -j MASQUERADE;
+END
 
-iptables -t nat -I POSTROUTING -s 10.11.11.1 -o $SERVER_PUB_NIC -j MASQUERADE
-iptables -I INPUT 1 -i wg0 -j ACCEPT
-iptables -I FORWARD 1 -i $SERVER_PUB_NIC -o wg0 -j ACCEPT
-iptables -I FORWARD 1 -i wg0 -o $SERVER_PUB_NIC -j ACCEPT
-iptables -I INPUT 1 -i $SERVER_PUB_NIC -p udp --dport 591 -j ACCEPT
-iptables -I INPUT 1 -i $SERVER_PUB_NIC -p tcp --dport 591 -j ACCEPT
-iptables-save > /etc/iptables.up.rules
-iptables-restore -t < /etc/iptables.up.rules
-netfilter-persistent save
-netfilter-persistent reload
+sleep 1
+echo -e "[ ${green}INFO$NC ] Setting up iptables..."
+sudo iptables -t nat -I POSTROUTING -s 10.11.11.1/24 -o $SERVER_PUB_NIC -j MASQUERADE
+sudo iptables -I INPUT 1 -i wg0 -j ACCEPT
+sudo iptables -I FORWARD 1 -i $SERVER_PUB_NIC -o wg0 -j ACCEPT
+sudo iptables -I FORWARD 1 -i wg0 -o $SERVER_PUB_NIC -j ACCEPT
+sudo iptables -I INPUT 1 -i $SERVER_PUB_NIC -p udp --dport 591 -j ACCEPT
+sudo iptables-save > /etc/iptables.up.rules
+sudo iptables-restore -t < /etc/iptables.up.rules
+sudo netfilter-persistent save >/dev/null 2>&1
+sudo netfilter-persistent reload >/dev/null 2>&1
 
-systemctl start "wg-quick@wg0"
-systemctl enable "wg-quick@wg0"
+sleep 1
+echo -e "[ ${green}INFO$NC ] Enable wireguard services..."
+systemctl enable "wg-quick@wg0" >/dev/null 2>&1
+systemctl start "wg-quick@wg0" >/dev/null 2>&1
+
 
 # Check if WireGuard is running
-systemctl is-active --quiet "wg-quick@wg0"
+systemctl is-active --quiet "wg-quick@wg0" >/dev/null 2>&1
 WG_RUNNING=$?
 
 # Tambahan
-cd /usr/bin
-wget -O addwg "https://${wisnuvpn}/addwg.sh"
-wget -O delwg "https://${wisnuvpn}/delwg.sh"
-wget -O renewwg "https://${wisnuvpn}/renewwg.sh"
-chmod +x addwg
-chmod +x delwg
-chmod +x renewwg
-cd
-rm -f /root/wg.sh
+
+wget -q -O /usr/bin/add-wg "https://raw.githubusercontent.com/inoyaksorojawi/gandring/master/wrguard/add-wg.sh" && chmod +x /usr/bin/add-wg
+wget -q -O /usr/bin/del-wg "https://raw.githubusercontent.com/inoyaksorojawi/gandring/master/wrguard/del-wg.sh" && chmod +x /usr/bin/del-wg
+wget -q -O /usr/bin/cek-wg "https://raw.githubusercontent.com/inoyaksorojawi/gandring/master/wrguard/cek-wg.sh" && chmod +x /usr/bin/cek-wg
+wget -q -O /usr/bin/renew-wg "https://raw.githubusercontent.com/inoyaksorojawi/gandring/master/wrguard/renew-wg.sh" && chmod +x /usr/bin/renew-wg
+wget -q -O /usr/bin/trial-wg "https://raw.githubusercontent.com/inoyaksorojawi/gandring/master/wrguard/trial-wg.sh" && chmod +x /usr/bin/trial-wg
+wget -q -O /usr/bin/port-wg "https://raw.githubusercontent.com/inoyaksorojawi/gandring/master/wrguard/port-wg.sh" && chmod +x /usr/bin/port-wg
+
+#wget -q -O /usr/bin/wg-menu "https://raw.githubusercontent.com/inoyaksorojawi/gandring/master/menu_all/wg-menu.sh" && chmod +x /usr/bin/wg-menu
+
+sleep 1
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+yellow "Wireguard install successfully..."
+sleep 5
+clear
+rm -f /root/wg.sh >/dev/null 2>&1
