6899a80d9d2399f70e1e7f814ba5efd1d310cb23
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-07-13 00:16:13 +0700

    Update addssr.sh

--- ssr/addssr.sh
+++ ssr/addssr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SL
+# wisnucokrosatrio
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -13,16 +13,6 @@ LIGHT='\033[0;37m'
 # ==========================================
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl ipinfo.io/ip | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck You"
-exit 0
-fi
-clear
 IP=$(wget -qO- ipinfo.io/ip);
 read -e -p "Username : " ssr_user
 CLIENT_EXISTS=$(grep -w $ssr_user /usr/local/shadowsocksr/akun.conf | wc -l)
@@ -31,7 +21,7 @@ echo ""
 echo -e "Username ${RED}${ssr_user}${NC} Already On VPS Please Choose Another"
 exit 1
 fi
-source /var/lib/crot/ipvps.conf
+source /var/lib/wisnucs/ipvps.conf
 if [[ "$IP2" = "" ]]; then
 domain=$(cat /etc/xray/domain)
 else
@@ -42,12 +32,12 @@ hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 lastport=$(cat /usr/local/shadowsocksr/mudb.json | grep '"port": ' | tail -n1 | awk '{print $2}' | cut -d "," -f 1 | cut -d ":" -f 1 )
 if [[ $lastport == '' ]]; then
-ssr_port=1443
+ssr_port=30000
 else
 ssr_port=$((lastport+1))
 fi
 ssr_password="$ssr_user"
-ssr_method="aes-256-cfb"
+ssr_method="aes-128-gcm"
 ssr_protocol="origin"
 ssr_obfs="plain"
 ssr_protocol_param="2"
@@ -63,25 +53,28 @@ echo -e "### $ssr_user $exp" >> /usr/local/shadowsocksr/akun.conf
 tmp1=$(echo -n "${ssr_password}" | base64 -w0 | sed 's/=//g;s/\//_/g;s/+/-/g')
 SSRobfs=$(echo ${ssr_obfs} | sed 's/_compatible//g')
 tmp2=$(echo -n "$IP:${ssr_port}:${ssr_protocol}:${ssr_method}:${SSRobfs}:${tmp1}/obfsparam=" | base64 -w0)
-ssr_link="ssr://${tmp2}"
+ssr_link="ssr://${tmp2}#$user"
 /etc/init.d/ssrmu restart
 systemctl restart ssrmu
 service cron restart
 IP=$(wget -qO- ifconfig.co);
 clear
 echo -e ""
-echo -e "======-SHADOWSOCKSR-======"
-echo -e "IP/Host     : ${IP}"
-echo -e "Domain      : $domain"
-echo -e "Port        : ${ssr_port}"
-echo -e "Password    : ${ssr_password}"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;46m    🔰 AKUN SHADOWSOCKS-RR 🔰     \e[m"   
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "IP/Host  : ${IP}"
+echo -e "Domain  : $domain"
+echo -e "Port  : ${ssr_port}"
+echo -e "Password  : ${ssr_password}"
 echo -e "Encryption  : ${ssr_method}"
-echo -e "Protocol    : ${Red_font_prefix}${ssr_protocol}"
-echo -e "Obfs        : ${Red_font_prefix}${ssr_obfs}"
+echo -e "Protocol  : ${Red_font_prefix}${ssr_protocol}"
+echo -e "Obfs  : ${Red_font_prefix}${ssr_obfs}"
 echo -e "Max Device  : ${ssr_protocol_param}"
-echo -e "Created     : $hariini"
-echo -e "Expired     : ${exp} "
-echo -e "=========================="
-echo -e "Link SSR    : ${ssr_link}"
-echo -e "=========================="
-echo -e "Script Mod By SL"
+echo -e "Created  : $hariini"
+echo -e "Expired  : ${exp} "
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "Link SSR  : ${ssr_link}"
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;46m  🔰LUXURY EDITION BY ZEROSSL🔰   \e[m"   
+echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
