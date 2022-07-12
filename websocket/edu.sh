76cc775d45d1842f42e1b839a8002d490c4d6036
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-07-13 02:06:44 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -79,7 +79,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/wstunnel 2087
+ExecStart=/usr/bin/python -O /usr/local/bin/wstunnel 590
 Restart=on-failure
 
 [Install]
