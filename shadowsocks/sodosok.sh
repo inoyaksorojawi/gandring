deea5d3412f9150dfeb6d47888b54f9de4a8c416
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-07-13 07:03:36 +0700

    Update sodosok.sh

--- shadowsocks/sodosok.sh
+++ shadowsocks/sodosok.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#shadowsocks-libev obfs install by wisnu cokro satrio
-# My Telegram : https://t.me/zerossl
+#shadowsocks-libev obfs install by wisnu
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -17,18 +16,21 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 
 # Link Hosting Kalian
 wisnuvpn="raw.githubusercontent.com/inoyaksorojawi/gandring/master/shadowsocks"
+
 source /etc/os-release
 OS=$ID
 ver=$VERSION_ID
+
 #Install_Packages
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "#############################################"
 echo "Install Paket..."
 apt-get install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libev-dev asciidoc xmlto automake -y
-echo "\e[1;31m Install Paket Selesai BUILD UP BY WISNU COKRO SATRIO \e[m"
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "Install Paket Selesai."
+echo "#############################################"
+
 #Install_Shadowsocks_libev
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "\e[1;31m Installing Shadowsocks-libev BY WISNU COKRO SATRIO....."
+echo "#############################################"
+echo "Install Shadowsocks-libev..."
 apt-get install software-properties-common -y
 if [[ $OS == 'ubuntu' ]]; then
 apt install shadowsocks-libev -y
@@ -45,10 +47,12 @@ apt update
 apt -t buster-backports install shadowsocks-libev -y
 apt -t buster-backports install simple-obfs -y
 fi
-echo "\e[1;31m Install Shadowsocks-libev Selesai.\e[m"
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+fi
+echo "Install Shadowsocks-libev Selesai."
+echo "#############################################"
+
 #Server konfigurasi
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "#############################################"
 echo "Konfigurasi Server."
 cat > /etc/shadowsocks-libev/config.json <<END
 {   
@@ -58,21 +62,21 @@ cat > /etc/shadowsocks-libev/config.json <<END
     "timeout":60,
     "method":"aes-256-cfb",
     "fast_open":true,
-    "nameserver":"1.1.1.1",
+    "nameserver":"8.8.8.8",
     "mode":"tcp_and_udp",
 }
 END
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "#############################################"
 
 #mulai ~shadowsocks-libev~ server
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "#############################################"
 echo "mulai ss server"
 systemctl enable shadowsocks-libev.service
 systemctl start shadowsocks-libev.service
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "#############################################"
 
 #buat client config
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "#############################################"
 echo "buat config obfs"
 cat > /etc/shadowsocks-libev.json <<END
 {
@@ -81,7 +85,7 @@ cat > /etc/shadowsocks-libev.json <<END
     "local_port":1080,
     "password":"",
     "timeout":60,
-    "method":"aes-128-gcm",
+    "method":"chacha20-ietf-poly1305",
     "mode":"tcp_and_udp",
     "fast_open":true,
     "plugin":"/usr/bin/obfs-local",
@@ -89,11 +93,16 @@ cat > /etc/shadowsocks-libev.json <<END
 }
 END
 chmod +x /etc/shadowsocks-libev.json
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "">>"/etc/shadowsocks-libev/akun.conf"
-echo -e "\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo "Menambahkan Perintah Shadowsocks-libev"
+echo "#############################################"
 
+echo -e "">>"/etc/shadowsocks-libev/akun.conf"
+
+echo "#############################################"
+echo "Menambahkan Perintah Shadowsocks-libev"
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 20000-20500 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 20501-22000 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+ip6tables-save > /etc/ip6tables.up.rules
 cd /usr/bin
 wget -O addss "https://${wisnuvpn}/addss.sh"
 wget -O delss "https://${wisnuvpn}/delss.sh"
