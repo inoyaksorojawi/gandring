d70302c9ae0ce67db99c7bbf0e2167ec08780a23
inoyaksorojawi <71470149+inoyaksorojawi@users.noreply.github.com>
2022-07-13 13:17:10 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -63,34 +63,6 @@ systemctl enable ws-ovpn
 systemctl restart ws-ovpn
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/wstunnel https://${wisnuvpn}/wstunnel.py
-chmod +x /usr/local/bin/wstunnel
-
-# Installing Service
-cat > /etc/systemd/system/wstunnel.service << END
-[Unit]
-Description=WEBSOCKET OVPN ROUTING DAM COLO PENGKOL BY SHANUM
-Documentation=https://t.me/zerossl
-After=network.target nss-lookup.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/wstunnel 590
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-END
-
-systemctl daemon-reload
-systemctl enable wstunnel
-systemctl restart wstunnel
-
-# Getting Proxy Template
 wget -q -O /usr/local/bin/ws-tls https://${wisnuvpn}/ws-tls.py
 chmod +x /usr/local/bin/ws-tls
 
@@ -117,3 +89,31 @@ END
 systemctl daemon-reload
 systemctl enable ws-tls
 systemctl restart ws-tls
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/wstunnel https://${wisnuvpn}/wstunnel.py
+chmod +x /usr/local/bin/wstunnel
+
+# Installing Service
+cat > /etc/systemd/system/wstunnel.service << END
+[Unit]
+Description=WEBSOCKET WIREGUARD ROUTING DAM COLO PENGKOL BY SHANUM
+Documentation=https://t.me/zerossl
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/wstunnel 590
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable wstunnel
+systemctl restart wstunnel
