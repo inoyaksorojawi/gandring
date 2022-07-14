1a347914727a9e0a280b06c7bfd113119bc1879f
pengkol <108908182+pengkol@users.noreply.github.com>
2022-07-14 11:17:06 +0700

    Update ws-ovpn.py
    Signed-off-by: pengkol <108908182+pengkol@users.noreply.github.com>

--- websocket/ws-ovpn.py
+++ websocket/ws-ovpn.py
@@ -1,4 +1,37 @@
 #!/usr/bin/python
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl="\033[0;1;33m"
+wh="\033[0m"
+m="\033[0;1;36m"
+y="\033[0;1;37m"
+yy="\033[0;1;32m"
+yl='\e[031;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+bg_red='\033[0;41m'
+bg_green='\033[0;42m'
+bg_yellow='\033[0;43m'
+bg_blue='\033[0;44m'
+bg_magenta='\033[0;45m'
+bg_cyan='\033[0;46m'
 import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
