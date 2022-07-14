37a2c610c65f3832bccc99aee50505593d448004
pengkol <108908182+pengkol@users.noreply.github.com>
2022-07-14 19:01:19 +0700

    Update ins-xray.sh
    Signed-off-by: pengkol <108908182+pengkol@users.noreply.github.com>

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -859,7 +859,7 @@ cat > /etc/trojan-go/config.json << END
   "local_addr": "0.0.0.0",
   "local_port": 2053,
   "remote_addr": "127.0.0.1",
-  "remote_port": 88,
+  "remote_port": 80,
   "log_level": 1,
   "log_file": "/var/log/trojan-go/trojan-go.log",
   "password": [
