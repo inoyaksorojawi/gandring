c5e912419942e13d3ce279752e1d8e1835348dfb
pengkol <108908182+pengkol@users.noreply.github.com>
2022-07-14 19:05:56 +0700

    Update ins-xray.sh
    Signed-off-by: pengkol <108908182+pengkol@users.noreply.github.com>

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -120,6 +120,20 @@ elif [[ $OS == 'debian' ]]; then
         sudo mv /tmp/nginx_signing.key /etc/apt/trusted.gpg.d/nginx_signing.asc
         sudo apt update 
         apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
+        # install webserver
+        rm /etc/nginx/sites-enabled/default
+        rm /etc/nginx/sites-available/default
+        curl https://${wisnuvpn}/nginx.conf > /etc/nginx/nginx.conf
+        curl https://${wisnuvpn}/vps.conf > /etc/nginx/conf.d/vps.conf
+        sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
+        useradd -m vps;
+        mkdir -p /home/vps/public_html
+        echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
+        chown -R www-data:www-data /home/vps/public_html
+        chmod -R g+rw /home/vps/public_html
+        cd /home/vps/public_html
+        wget -O /home/vps/public_html/index.html "https://${wisnuvpn}/index.html"
+        /etc/init.d/nginx restart
         systemctl daemon-reload
         systemctl enable nginx
 fi
