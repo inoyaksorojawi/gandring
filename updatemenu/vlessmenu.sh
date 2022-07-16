2afa49974b279bbc6c9e3357662de4c38ce9ca4b
pengkol <108908182+pengkol@users.noreply.github.com>
2022-07-16 09:47:08 +0700

    Update vlessmenu.sh
    Signed-off-by: pengkol <108908182+pengkol@users.noreply.github.com>

--- updatemenu/vlessmenu.sh
+++ updatemenu/vlessmenu.sh
@@ -11,35 +11,30 @@ echo -e "02\e[1;33m) Hapus Akun VLESS WEBSOCKET  \e[m"
 echo -e "03\e[1;33m) Perpanjang Masa Aktif Akun VLESS WEBSOCKET  \e[m"
 echo -e "04\e[1;33m) Cek User Login VLESS WEBSOCKET  \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "05\e[1;33m) Buat Akun TROJAN H2C \e[m"
-echo -e "06\e[1;33m) Hapus Akun TROJAN H2C \e[m"
-echo -e "07\e[1;33m) Perpanjang Masa Aktif Akun TROJAN H2C \e[m"
-echo -e "08\e[1;33m) Cek User Login Trojan H2C \e[m"
+echo -e "05\e[1;33m) Buat Akun VLESS GRPC \e[m"
+echo -e "06\e[1;33m) Hapus Akun VLESS GRPC \e[m"
+echo -e "07\e[1;33m) Perpanjang Masa Aktif Akun VLESS GRPC \e[m"
+echo -e "08\e[1;33m) Cek User Login VLESS GRPC \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "09\e[1;33m) Buat Akun TROJAN GRPC \e[m"
-echo -e "10\e[1;33m) Hapus Akun TROJAN GRPC \e[m"
-echo -e "11\e[1;33m) Perpanjang Masa Aktif Akun TROJAN GRPC \e[m"
-echo -e "12\e[1;33m) Cek User Login TROJAN GRPC \e[m"
+echo -e "09\e[1;33m) Buat Akun VLESS H2C \e[m"
+echo -e "10\e[1;33m) Hapus Akun VLESS H2C \e[m"
+echo -e "11\e[1;33m) Perpanjang Masa Aktif Akun VLESS H2C \e[m"
+echo -e "12\e[1;33m) Cek User Login VLESS H2C \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "13\e[1;33m) Buat Akun TROJAN WEBSOCKET \e[m"
-echo -e "14\e[1;33m) Hapus Akun TROJAN WEBSOCKET  \e[m"
-echo -e "15\e[1;33m) Perpanjang Masa Aktif Akun TROJAN WEBSOCKET  \e[m"
-echo -e "16\e[1;33m) Cek User Login TROJAN WEBSOCKET  \e[m"
+echo -e "13\e[1;33m) Buat Akun VLESS XTLS \e[m"
+echo -e "14\e[1;33m) Hapus Akun VLESS XTLS  \e[m"
+echo -e "15\e[1;33m) Perpanjang Masa Aktif Akun VLESS XTLS  \e[m"
+echo -e "16\e[1;33m) Cek User Login VLESS XTLS  \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "17\e[1;33m) Buat Akun TROJAN HTTP \e[m"
-echo -e "18\e[1;33m) Hapus Akun TROJAN HTTP \e[m"
-echo -e "19\e[1;33m) Perpanjang Masa Aktif Akun TROJAN HTTP \e[m"
-echo -e "20\e[1;33m) Cek User Login TROJAN HTTP \e[m"
+echo -e "17\e[1;33m) Buat Akun VLESS HTTP \e[m"
+echo -e "18\e[1;33m) Hapus Akun VLESS HTTP \e[m"
+echo -e "19\e[1;33m) Perpanjang Masa Aktif Akun VLESS HTTP \e[m"
+echo -e "20\e[1;33m) Cek User Login VLESS HTTP \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "21\e[1;33m) Buat Akun TROJAN GO \e[m"
-echo -e "22\e[1;33m) Hapus Akun TROJAN GO \e[m"
-echo -e "23\e[1;33m) Perpanjang Masa Aktif Akun TROJAN GO \e[m"
-echo -e "24\e[1;33m) Cek User Login TROJAN GO \e[m"
-echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "25\e[1;33m) Buat Akun TROJAN QUIC \e[m"
-echo -e "26\e[1;33m) Hapus Akun TROJAN QUIC \e[m"
-echo -e "27\e[1;33m) Perpanjang Masa Aktif Akun TROJAN QUIC \e[m"
-echo -e "28\e[1;33m) Cek User Login TROJAN QUIC \e[m"
+echo -e "21\e[1;33m) Buat Akun VLESS QUIC \e[m"
+echo -e "22\e[1;33m) Hapus Akun VLESS QUIC \e[m"
+echo -e "23\e[1;33m) Perpanjang Masa Aktif Akun VLESS QUIC \e[m"
+echo -e "24\e[1;33m) Cek User Login VLESS QUIC \e[m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "00\e[1;31m) Menu Utama \e[m"
 echo -e "xx\e[1;31m) Keluar \e[m"
@@ -50,88 +45,76 @@ read -p "Silahkan Pilih Angka[ 1 - 0 ] : " menu
 echo -e ""
 case $menu in
 1)
-addtrojanxtls
+addvless
 ;;
 2)
-deltrojanxtls
+delvless
 ;;
 3)
-renewtrojanxtls
+renewvless
 ;;
 4)
-cektrojanxtls
+cekvless
 ;;
 5)
-addtrojanhdua
+addvlessgrpc
 ;;
 6)
-deltrojanhdua
+delvlessgrpc
 ;;
 7)
-renewtrojanhdua
+renewvlessgroc
 ;;
 8)
-cektrojanhdua
+cekvlessgrpc
 ;;
 9)
-addtrojangrpc
+addvlesshdua
 ;;
 10)
-deltrojangrpc
+delvlesshdua
 ;;
 11)
-renewtrojangrpc
+renewvlesshdua
 ;;
 12)
-cektrojangrpc
+cekvlesshdua
 ;;
 13)
-addtrojanwss
+addvlessxtls
 ;;
 14)
-deltrojanwss
+delvlessxtls
 ;;
 15)
-renewtrojanwss
+renewvlessxtls
 ;;
 16)
-cektrojanwss
+cekvlessxtls
 ;;
 17)
-addtrojanhttp
+addvlesshttp
 ;;
 18)
-deltrojanhttp
+delvlesshttp
 ;;
 19)
-renewtrojanhttp
+renewvlesshttp
 ;;
 20)
-cektrojanhttp
+cekvlesshttp
 ;;
 21)
-addtrgo
+addvlessquic
 ;;
 22)
-deltrgo
+delvlessquic
 ;;
 23)
-renewtrgo
+renewvlessquic
 ;;
 24)
-cektrgo
-;;
-25)
-addtrojanquic
-;;
-26)
-deltrojanquic
-;;
-27)
-renewtrojanquic
-;;
-28)
-cektrojanquic
+cekvlessquic
 ;;
 0)
 menu
